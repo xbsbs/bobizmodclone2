2417d9efc8b86e1cc1030dbea297e94e7ece47d5
Noureddine Ouafy <111184442+soufiansaf@users.noreply.github.com>
2023-01-24 22:39:05 +0100

    Update nv-a.js

--- plugins/nv-a.js
+++ plugins/nv-a.js
@@ -3,8 +3,8 @@ if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
 let vn = './media/a.mp3'
 conn.sendPresenceUpdate('recording', m.chat)
 conn.sendMessage(m.chat, { audio: { url: vn }, seconds: '1934.4', ptt: true, mimetype: 'audio/mpeg', fileName: `a.mp3` }, { quoted: m })}
-handler.customPrefix = /ª|a|A/
-handler.command = /^(a|ª|A?$)/
+handler.customPrefix = /ª|a|salam|A/
+handler.command = /^(a|salam|ª|A?$)/
 export default handler
 
 /*
